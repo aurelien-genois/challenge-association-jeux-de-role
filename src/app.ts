e4c97a0745e336c60b3af1ff305b5d1d40dc881c
aurelien.genois <aurelien.genois@gmail.com>
2025-12-15 16:34:17 +0100

    🌟 feat(app): Add Morgan HTTP request logger middleware
    Changelog: feature

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,15 @@ import express from "express";
 import router from "./routes/index.route";
 import cookieParser from "cookie-parser";
 import { globalErrorHandler } from "./middlewares/globalErrorHandler.middleware";
+import { loggerMiddleware } from "./middlewares/httpLogger.middleware";
+import morgan from "morgan";
 
 const app = express();
 
 app.use(cookieParser());
+
+app.use(morgan("dev")); // Concise output colored by response status for development use. default output stream
+app.use(loggerMiddleware);
 
 app.use(express.json());
 
