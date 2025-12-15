3fde7a7b94c0eab183e9f56f23ec2299dc21e73c
aurelien.genois <aurelien.genois@gmail.com>
2025-12-15 17:14:31 +0100

    🌟 feat(app): Add Body Sanitizer middleware
    Changelog: feature

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,7 @@ import morgan from "morgan";
 import { config } from "../server.config";
 import helmet from "helmet";
 import cors from "cors";
+import bodySanitizer from "./middlewares/bodySanitizer.middleware";
 
 const app = express();
 
@@ -16,12 +17,19 @@ app.use(helmet());
 // ensure origin matches your frontend URLs and credentials: true is set if you rely on cookie
 app.use(cors({ origin: config.server.allowedOrigins, credentials: true }));
 
+app.use(express.json()); // Parses incoming requests with Content-Type: application/json (converts raw JSON into req.body)
+
+// Parses requests with Content-Type: application/x-www-form-urlencoded
+// "extended" allows rich objects/arrays via the qs library
+// Useful if: traditionnal form submissions and the API support clients that send data as URL‑encoded instead of JSON
+// app.use(express.urlencoded({ extended: true }));
+
+app.use(bodySanitizer); // Prevents XSS or injection attacks
+
 app.use(cookieParser());
 
 app.use(morgan("dev")); // Concise output colored by response status for development use. default output stream
 app.use(loggerMiddleware);
-
-app.use(express.json());
 
 app.use("/", router);
 
