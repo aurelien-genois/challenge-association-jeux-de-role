9cb855f33cbfc24ba583840e0731302444617e78
aurelien.genois <aurelien.genois@gmail.com>
2025-12-31 15:08:59 +0100

    📚 docs(app): Add welcome message for API root url
    Changelog: documentation

--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -16,4 +16,11 @@ router.use("/auth", auth);
 router.use("/users", users);
 router.use("/games", games);
 
+router.use("/", (_req, res) => {
+  res.status(200).json({
+    message: "Bienvenue dans l'API AJDR",
+    timestamp: new Date().toISOString(),
+  });
+});
+
 export default router;
